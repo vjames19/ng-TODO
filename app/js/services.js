9f65d6ac3b3ad1a1be1d049c7b4c87ad62aede41
Victor J <victor.reventos1@gmail.com>
2013-05-23 12:13:25 -0400

    changed from static to using mongo lab

--- app/js/services.js
+++ app/js/services.js
@@ -5,36 +5,51 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
+angular.module('myApp.services', ['ngResource']).
     value('version', '0.1');
 
-app.factory('Todos', function (Utils, Models) {
-    var todos = {active: [], completed: []};
-    todos.removeTodo = function (text, arr) {
-        var index = Utils.findWithinArray(arr, 'text', text);
-        arr.splice(index, 1);
-    }
+app.factory('Todo', function ($resource) {
+    var todo = $resource('https://api.mongolab.com/api/1/databases' +
+        '/victortestingmongolab/collections/todos/:id',
+        { apiKey: 'qcukEFdSs2P_tqmbzC1u73GF-qqgVyai' }, {
+            update: { method: 'PUT' }
+        }
+    );
+    todo.prototype.$remove = function () {
+        todo.remove({id: this._id.$oid});
+    };
 
-    todos.findTodo = function (text, arr) {
-        return Utils.findWithinArray(arr, 'text', text);
-    }
+    todo.prototype.$update = function () {
+        return todo.update({id: this._id.$oid}, angular.extend({}, this, {_id: undefined}));
+    };
 
-    todos.addTodo = function (text, arr) {
-        arr.push(new Models.Todo(text, false))
-    }
+    todo.prototype.done = false;
 
-    return todos;
+    return todo;
 });
 
 app.factory('Utils', function () {
     var utils = {
-        findWithinArray: function (array, property, value) {
-            for (var i = 0; i < array.length; i += 1) {
+        findWithinArray: function (array, property, value, index) {
+            for (var i = (index || 0); i < array.length; i += 1) {
                 if (array[i][property] === value) {
                     return i;
                 }
             }
             return -1;
+        },
+
+        filterOnProp: function (array, property, value) {
+            var arr = new Array();
+            var i = -1, len = array.length;
+            do {
+                i = this.findWithinArray(array, property, value, i + 1);
+                if (i > -1) {
+                    arr.push(array[i]);
+                }
+            } while (i >= 0 && i < len);
+
+            return arr;
         }
     };
 
